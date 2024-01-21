9f32ff19c1e7cbebf94da996ae34616d5e001894
es9dpw <es9dpw999gmail.com>
2024-01-21 18:01:16 +0000

    signup/login done

--- OTS/Consumers.cs
+++ OTS/Consumers.cs
@@ -2,6 +2,7 @@ using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Collections;
+using System.Linq;
 
 namespace OTS.Consumers
 {
@@ -10,10 +11,9 @@ namespace OTS.Consumers
     /// </summary>
     public class Consumer
     {
-        bool exit = false;
-        bool LoggedIn = false;
+        bool loggedIn = false;
         string account;
-        public string email;
+        public string email = ".";
         public string password;
         public string name;
         public ArrayList emails = new ArrayList();
@@ -22,17 +22,15 @@ namespace OTS.Consumers
 
         public void Run()
         {
-            Console.Write("Please select Log In or Sign Up.\n1. Log In\n2. Sign Up");
-            account = Console.ReadLine();
-           
-            while (exit == false){
+            while (loggedIn == false){
+                Console.Write("Please select Log In or Sign Up.\n1. Log In\n2. Sign Up");
+                account = Console.ReadLine();
+
                 if (string.Equals(account, "1") || string.Equals(account, "Log In")){
                     LogIn();
-                    exit = true;
                 }
                 else if (string.Equals(account, "2") || string.Equals(account, "Sign Up")){
                     SignUp();
-                    exit = true;
                 }
                 else{
                     Console.WriteLine("Invalid option, please try again.");
@@ -42,35 +40,41 @@ namespace OTS.Consumers
 
         public void LogIn()
         {
-            while (LoggedIn == false){
-                Console.Write("Please enter your email: ");
-                email = Console.ReadLine();
-                Console.Write("Please enter your password: ");
-                password = Console.ReadLine();
-                for(int i = 0 ; i < emails.Count; i++){
-                    if (emails[i] == email & passwords[i] == password){
-                        name = names[i].ToString();;
-                        Console.WriteLine("You have logged in.\n");
-                        LoggedIn = true;
-                        break;
-                    }
+            Console.Write("Please enter your email: ");
+            email = Console.ReadLine();
+            Console.Write("Please enter your password: ");
+            password = Console.ReadLine();
+            for(int i = 0 ; i < emails.Count; i++){
+                if (string.Equals(emails[i], email) & string.Equals(passwords[i], password)){
+                    name = names[i].ToString();;
+                    Console.WriteLine("You have logged in.\n");
+                    loggedIn = true;
+                    break;
                 }
-                if (LoggedIn == false){
-                    Console.WriteLine("Email or password is invalid, Please try again.");
-                }
+            }
+            if (loggedIn == false){
+                Console.WriteLine("Email or password is invalid, Please try again.\n");
             }
         }
 
         public void SignUp()
         {
-            Console.Write("Please enter your email: ");
-            emails.Add(Console.ReadLine());
+            while (email.Contains("@") == false){
+                Console.Write("Please enter your email: ");
+                email = Console.ReadLine();
+                if (email.Contains("@") == false){
+                    Console.WriteLine("Email is not valid, please try again");
+                }
+            }
+            emails.Add(email);
             Console.Write("Please enter your password: ");
-            passwords.Add(Console.ReadLine());
+            password = Console.ReadLine();
+            passwords.Add(password);
             Console.Write("Please enter your name: ");
-            names.Add(Console.ReadLine());
-            Console.WriteLine("Your account was created and you bave been logged in.\n");
-            LoggedIn = true;
+            name = Console.ReadLine();
+            names.Add(name);
+            Console.WriteLine("Your account was created and you have been logged in.\n");
+            loggedIn = true;
         }
     }
 }
