e37647fddf5d8f049ef769216a5b3ba4d7572001
es9dpw <es9dpw999gmail.com>
2024-01-21 16:12:51 +0000

    commit

--- OTS/Program.cs
+++ OTS/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace OTS
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
