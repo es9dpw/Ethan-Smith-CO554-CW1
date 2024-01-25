0b3b2d3baf643ded8820ba26a5dd6d1dc6fa0c85
es9dpw <es9dpw999gmail.com>
2024-01-25 14:58:30 +0000

    commented program

--- OTS/Program.cs
+++ OTS/Program.cs
@@ -5,10 +5,13 @@ using System;
 namespace OTS
 {
     /// <summary>
-    /// 
+    /// This class calls the methods from the other classes when the porgram is run.
     /// </summary>
     public static class Program
     {
+        /// <summary>
+        /// This method is the main method for this class and it calls the methods from other classes.
+        /// </summary>
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Online Ticketing System for The Bucks Centre for the Performing Arts\n");
